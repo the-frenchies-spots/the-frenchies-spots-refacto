6a79e24ed2e6ad9496aab34b5a38fca40dc0c3ec
JF-PS <jfps.dev21@gmail.com>
2023-08-27 11:11:32 +0200

    landing-page

--- apps/frenchies-spots-api/src/main.ts
+++ apps/frenchies-spots-api/src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
